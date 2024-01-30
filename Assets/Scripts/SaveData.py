6b6830ee526b0beaf0ad3ab8b98bf9bc1d3f0b32
Landon Montecalvo <115672301+ljmontecalvo@users.noreply.github.com>
2024-01-30 13:10:16 -0500

    Added Python Reader
    The python reader will eventually upload the data from the .csv file to Salesforce.

--- Assets/Scripts/SaveData.py
+++ Assets/Scripts/SaveData.py
@@ -3,9 +3,19 @@ import csv
 
 directory = os.getcwd()
 
-print(directory)
+rows = []
 
-with open(directory + "\\test.csv", "w", newline="") as file:
-    print("Hello, World!")
-    writer = csv.writer(file)
-    writer.writerow(["Test Test 123"])
+org_name = ""
+item_values = []
+
+with open(directory + "\\Assets\\SAVE_DATA\\export.csv", "r", newline="", encoding="UTF-8") as file:
+    reader = csv.reader(file)
+    rows = list(reader)
+    
+org_name = rows[0][0]
+
+for i in range(1, len(rows) - 1):
+    item_values.append(rows[i][0])
+
+print(org_name)
+print(item_values)
